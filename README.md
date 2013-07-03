78e193a02f37f5e941eb3045eeba8384536f34b6
tokuhirom <tokuhirom@gmail.com>
2013-07-03 17:26:32 +0900

    updated documents.

--- README.md
+++ README.md
@@ -1,14 +1,42 @@
 # NAME
 
-Unqlite - It's new $module
+Unqlite - Perl bindings for Unqlite
 
 # SYNOPSIS
 
     use Unqlite;
 
+    my $db = Unqlite->open('foo.db');
+    $db->kv_store('foo', 'bar');
+    say $db->kv_fetch('foo'); # => bar
+    $db->kv_delete('foo');
+    undef $db; # close database
+
 # DESCRIPTION
 
-Unqlite is ...
+UnQLite is a in-process software library which implements a self-contained, serverless, zero-configuration, transactional NoSQL database engine. UnQLite is a document store database similar to MongoDB, Redis, CouchDB etc. as well a standard Key/Value store similar to BerkeleyDB, LevelDB, etc.  
+
+This module is Perl5 binding for Unqlite.
+
+If you want to know more information about Unqlite, see [http://unqlite.org/](http://unqlite.org/).
+
+# METHODS
+
+- `my $db = Unqlite->open('foo.db'[, $mode]);`
+
+    Open the database.
+
+- `$db->kv_store($key, $value);`
+
+    Store the entry to database.
+
+- `$db->kv_fetch($key);`
+
+    Fetch data from database.
+
+- `$db->kv_delete($key);`
+
+    Delte ` $key ` from database.
 
 # LICENSE
 
--- lib/Unqlite.pm
+++ lib/Unqlite.pm
@@ -15,15 +15,47 @@ __END__
 
 =head1 NAME
 
-Unqlite - It's new $module
+Unqlite - Perl bindings for Unqlite
 
 =head1 SYNOPSIS
 
     use Unqlite;
 
+    my $db = Unqlite->open('foo.db');
+    $db->kv_store('foo', 'bar');
+    say $db->kv_fetch('foo'); # => bar
+    $db->kv_delete('foo');
+    undef $db; # close database
+
 =head1 DESCRIPTION
 
-Unqlite is ...
+UnQLite is a in-process software library which implements a self-contained, serverless, zero-configuration, transactional NoSQL database engine. UnQLite is a document store database similar to MongoDB, Redis, CouchDB etc. as well a standard Key/Value store similar to BerkeleyDB, LevelDB, etc.  
+
+This module is Perl5 binding for Unqlite.
+
+If you want to know more information about Unqlite, see L<http://unqlite.org/>.
+
+=head1 METHODS
+
+=over 4
+
+=item C<< my $db = Unqlite->open('foo.db'[, $mode]); >>
+
+Open the database.
+
+=item C<< $db->kv_store($key, $value); >>
+
+Store the entry to database.
+
+=item C<< $db->kv_fetch($key); >>
+
+Fetch data from database.
+
+=item C<< $db->kv_delete($key); >>
+
+Delte C< $key > from database.
+
+=back
 
 =head1 LICENSE
 
